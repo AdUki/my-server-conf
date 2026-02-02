b857bef6302897951a7adc01122b5196e5eddce3
Simon Mikuda <simon.mikuda@gmail.com>
2026-02-02 23:50:35 +0100

    Update do-backup.sh

--- immich/do-backup.sh
+++ immich/do-backup.sh
@@ -2,18 +2,21 @@
 
 set -e
 
-source .env
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+
+source "$SCRIPT_DIR/.env"
 
 if [[ -t 1 ]]; then
 	INTERACTIVE=1
-	FLAGS="--progress --verbose"
+	DOCKER_FLAGS="-t"
+	BORG_FLAGS="--progress --verbose"
 fi
 
-if [ $INTERACTIVE ]; then echo Backup Immich database; fi
-docker exec -t immich_postgres pg_dumpall --clean --if-exists --username=$DB_USERNAME > "$UPLOAD_LOCATION"/database-backup/immich-database.sql
+if [ -n "$INTERACTIVE" ]; then echo Backup Immich database; fi
+docker exec $DOCKER_FLAGS immich_postgres pg_dumpall --clean --if-exists --username="$DB_USERNAME" > "$UPLOAD_LOCATION"/database-backup/immich-database.sql
 
-if [ $INTERACTIVE ]; then echo Append to local Borg repository; fi
-borg create $FLAGS "$BACKUP_PATH/immich-borg::{now}" "$UPLOAD_LOCATION" --exclude "$UPLOAD_LOCATION"/thumbs/ --exclude "$UPLOAD_LOCATION"/encoded-video/
-borg prune $FLAGS --keep-weekly=4 --keep-monthly=3 "$BACKUP_PATH"/immich-borg
-borg compact $FLAGS "$BACKUP_PATH"/immich-borg
-if [ $INTERACTIVE ]; then echo Append to local Borg repository; fi
+if [ -n "$INTERACTIVE" ]; then echo Append to local Borg repository; fi
+borg create $BORG_FLAGS "$BACKUP_PATH/immich-borg::{now}" "$UPLOAD_LOCATION" --exclude "$UPLOAD_LOCATION"/thumbs/ --exclude "$UPLOAD_LOCATION"/encoded-video/
+borg prune $BORG_FLAGS --keep-weekly=4 --keep-monthly=3 "$BACKUP_PATH"/immich-borg
+borg compact $BORG_FLAGS "$BACKUP_PATH"/immich-borg
+if [ -n "$INTERACTIVE" ]; then echo Append to local Borg repository; fi
